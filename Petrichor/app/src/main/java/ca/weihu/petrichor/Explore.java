6c8eb755ecf634510befe7abd232632c3667434e
divyang1998 <divyang1998@gmail.com>
2017-11-01 16:33:29 -0400

    I added the today activity

--- Petrichor/app/src/main/java/ca/weihu/petrichor/Explore.java
+++ Petrichor/app/src/main/java/ca/weihu/petrichor/Explore.java
@@ -1,7 +1,9 @@
 package ca.weihu.petrichor;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Explore extends AppCompatActivity {
 
@@ -9,5 +11,23 @@ public class Explore extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_explore);
+    }
+
+    public void OnWeekClick(View view) {
+//Application Context and Activity
+        Intent intent = new Intent(getApplicationContext(), Week.class);
+        startActivityForResult (intent,0);
+    }
+
+    public void OnMonthClick(View view) {
+//Application Context and Activity
+        Intent intent = new Intent(getApplicationContext(), Month.class);
+        startActivityForResult (intent,0);
+    }
+
+    public void OnYearClick(View view) {
+//Application Context and Activity
+        Intent intent = new Intent(getApplicationContext(), Year.class);
+        startActivityForResult (intent,0);
     }
 }
