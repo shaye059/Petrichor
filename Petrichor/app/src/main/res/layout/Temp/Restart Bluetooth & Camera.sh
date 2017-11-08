d24e21e618083021d49350d2d5a16d2f20b2b42e
Wei Hu <wei@weihu.ca>
2017-11-08 15:17:23 -0500

    Capitalization

--- Petrichor/app/src/main/res/layout/Temp/Restart Bluetooth & Camera.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo launchctl stop com.apple.blued
-sudo launchctl start com.apple.blued
-
-sudo kextunload -b com.apple.iokit.BroadcomBluetoothHostControllerUSBTransport
-sudo kextload -b com.apple.iokit.BroadcomBluetoothHostControllerUSBTransport
-
-sudo killall VDCAssistant
