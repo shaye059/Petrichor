93f4da7ffabd08598e1498e90c895ffaa173f0ba
Wei Hu <wei@weihu.ca>
2017-11-10 19:04:03 -0500

    Last Safe Version

---
+++ Petrichor/app/src/main/java/ca/weihu/petrichor/Month.java
@@ -0,0 +1,13 @@
+package ca.weihu.petrichor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Month extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_month);
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# petrichor
+
+We propose a novel journal-keeping application that allows users to look back and cherish their most prized memories.
