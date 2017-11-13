a97948029c0d805e00d3dbc58a17fba2eddfd4d8
divyang1998 <divyang1998@gmail.com>
2017-11-13 02:37:19 -0500

    Divyang

--- Petrichor/app/src/main/java/ca/weihu/petrichor/MainActivity.java
+++ Petrichor/app/src/main/java/ca/weihu/petrichor/MainActivity.java
@@ -1,13 +1,25 @@
 package ca.weihu.petrichor;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class  MainActivity extends AppCompatActivity {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void OnTodayButton(View view) {
+        Intent in = new Intent(getApplicationContext(), Today.class);
+        startActivity(in);
+    }
+
+    public void OnExploreButton(View view) {
+        Intent intent = new Intent(getApplicationContext(), Explore.class);
+        startActivity(intent);
     }
 }
--- Petrichor/app/src/main/java/ca/weihu/petrichor/Random.java
+++ Petrichor/app/src/main/java/ca/weihu/petrichor/Random.java
@@ -1,8 +1,13 @@
 package ca.weihu.petrichor;
 
-/**
- * Created by divya on 11/10/2017.
- */
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
-public class Random {
+public class Random extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_randomday);
+    }
 }
