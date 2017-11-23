8ced3978b9bdf21027ac9f4c9a74dca32276e8a6
divyang1998 <divyang1998@gmail.com>
2017-11-23 01:35:12 -0500

    finally

--- Petrichor/app/src/main/java/ca/weihu/petrichor/SharedMemory.java
+++ Petrichor/app/src/main/java/ca/weihu/petrichor/SharedMemory.java
@@ -1,5 +1,6 @@
 package ca.weihu.petrichor;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -10,7 +11,8 @@ public class SharedMemory extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_mentionedhighlights);getWindow().getDecorView().setSystemUiVisibility(
+        setContentView(R.layout.activity_mentionedhighlights);
+        getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
