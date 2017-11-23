0595d7dc562d50b0716c4b90f2e475b49a99b7c3
divyang1998 <divyang1998@gmail.com>
2017-11-22 21:50:37 -0500

    login screen added

--- Petrichor/app/src/main/java/ca/weihu/petrichor/Home.java
+++ Petrichor/app/src/main/java/ca/weihu/petrichor/Home.java
@@ -10,7 +10,10 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_home);getWindow().getDecorView().setSystemUiVisibility(
+        setContentView(R.layout.activity_home);
+
+
+        getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
