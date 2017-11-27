f0cf24bacce68558c5075a94a0a491e09150d2f5
srikarkovvali <srikar.kv@gmail.com>
2017-11-26 19:42:32 -0500

    Commiting Working Authentication - Srikar

--- Petrichor/app/src/main/java/ca/weihu/petrichor/AccountLogin.java
+++ Petrichor/app/src/main/java/ca/weihu/petrichor/AccountLogin.java
@@ -3,22 +3,40 @@ package ca.weihu.petrichor;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Patterns;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
-public class AccountLogin extends AppCompatActivity implements View.OnClickListener {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+
+public class AccountLogin extends AppCompatActivity {
 
     private RelativeLayout relLayout = null;
+    FirebaseAuth mAuth;
+    EditText editTextUsername, editTextPassword;
+    ProgressBar progressBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_login);
+        mAuth = FirebaseAuth.getInstance();
+
 
         hideSystemUI();
 
@@ -39,16 +57,56 @@ public class AccountLogin extends AppCompatActivity implements View.OnClickListe
                 return true;
             }
         });
-        findViewById(R.id.btnAccCreate).setOnClickListener(this);
+
+        progressBar = (ProgressBar) findViewById(R.id.progressbar);
+        editTextUsername = (EditText) findViewById(R.id.editTextLoginUsername);
+        editTextPassword = (EditText) findViewById(R.id.editTextLoginPassword);
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.btnAccCreate:
-                startActivity(new Intent(this, AccountCreate.class));
-                break;
+    private void userLogin(){
+
+
+        String username = editTextUsername.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if (username.isEmpty()){
+            editTextUsername.setError("Email is required");
+            editTextUsername.requestFocus();
+            return;
         }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(username).matches()){
+            editTextUsername.setError("Please enter a valid email");
+            editTextUsername.requestFocus();
+            return;
+        }
+        if (password.isEmpty()){
+            editTextPassword.setError("Password is required");
+            editTextPassword.requestFocus();
+            return;
+        }
+        if (password.length()<6){
+            editTextPassword.setError("Length should be 6");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+
+        mAuth.signInWithEmailAndPassword(username,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                progressBar.setVisibility(View.GONE);
+                if (task.isSuccessful()){
+                    Intent intent = new Intent(getApplicationContext(), Home.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), "Some error occured", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     /**
@@ -79,8 +137,7 @@ public class AccountLogin extends AppCompatActivity implements View.OnClickListe
     }
 
     public void onLogin(View view) {
-        Intent in = new Intent(getApplicationContext(), Home.class);
-        startActivity(in);
+        userLogin();
     }
 
     public void onAccountCreate(View view) {
