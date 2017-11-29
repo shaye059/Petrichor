629b3e1e8b8f88ff8ff64dea612494687a7f4337
divyang1998 <divyang1998@gmail.com>
2017-11-29 02:07:59 -0500

    New Look

--- Petrichor/app/src/main/java/ca/weihu/petrichor/TimePeriod.java
+++ Petrichor/app/src/main/java/ca/weihu/petrichor/TimePeriod.java
@@ -16,12 +16,34 @@ public class TimePeriod {
         DAY, WEEK, MONTH, YEAR
     }
 
+    private void TimePeriod(TimePeriodLabel label, int identifier, TimePeriod parent){
+        this.label=label;
+        this.identifier=identifier;
+        this.parent=parent;
+    }
+
     public void setType(TimePeriodLabel x) {
         label = x;
     }
 
     public void setIdentifier(int x) {
         identifier = x;
+    }
+
+    public void setParent(TimePeriod parent){
+        this.parent=parent;
+    }
+
+    public TimePeriodLabel getLabel(){
+        return label;
+    }
+
+    public int getIdentifier(){
+        return identifier;
+    }
+
+    public TimePeriod getParent(){
+        return parent;
     }
 
 }
