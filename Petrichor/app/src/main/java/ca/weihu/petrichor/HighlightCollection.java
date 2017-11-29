8125db55ed2166143ccbdc381ae473d6ef5b17c9
Tri-An Nguyen <tnguy186@uottawa.ca>
2017-11-28 22:51:13 -0500

    created new classes according to UML

---
+++ Petrichor/app/src/main/java/ca/weihu/petrichor/HighlightCollection.java
@@ -0,0 +1,8 @@
+package ca.weihu.petrichor;
+
+/**
+ * Created by Tri-An on November 28, 2017.
+ */
+
+public interface HighlightCollection {
+}
---
+++ Petrichor/app/src/main/java/ca/weihu/petrichor/SharedCollection.java
@@ -0,0 +1,10 @@
+package ca.weihu.petrichor;
+
+import HighlightCollection;
+
+/**
+ * Created by Tri-An on November 28, 2017.
+ */
+
+public class SharedCollection implements HighlightCollection {
+}
