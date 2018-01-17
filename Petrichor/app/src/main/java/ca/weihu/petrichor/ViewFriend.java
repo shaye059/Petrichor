0f90c437935c217587079e8df9db2016b40034a7
shay059 <spencer.hayes.laverdiere@gmail.com>
2018-01-16 19:48:11 -0500

    Fixed a bug that made app crash when pressing a non menu area of a friend's explore page

--- Petrichor/app/src/main/java/ca/weihu/petrichor/ViewFriend.java
+++ Petrichor/app/src/main/java/ca/weihu/petrichor/ViewFriend.java
@@ -20,7 +20,6 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 
 public class ViewFriend extends AppCompatActivity {
-    private RelativeLayout relLayout = null;
     private String thisEmail;
     private String thisName;
     private TextView friendName;
@@ -41,20 +40,7 @@ public class ViewFriend extends AppCompatActivity {
         friendName = (TextView) findViewById(R.id.textViewFriend);
 
 
-        relLayout = (RelativeLayout) findViewById(R.id.relativeLayoutFriend);
 
-        relLayout.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-
-                InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
-                imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
-
-                hideSystemUI();
-
-                return true;
-            }
-        });
 
         userRef = FirebaseDatabase.getInstance().getReference().child("Account");
 
@@ -107,24 +93,7 @@ public class ViewFriend extends AppCompatActivity {
         onBackPressed();
     }
 
-    // P R E V E N T I N G  E X I T I N G  F U L L S C R E E N
 
-    // checks if window has focus
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-
-        super.onWindowFocusChanged(hasFocus);
-
-        // When the window gains focus, hide the system UI.
-        if (hasFocus) {
-            hideSystemUI();
-
-            // When the window loses focus (e.g. the action overflow is shown),
-            // cancel any pending hide action.
-        } else {
-            hideSystemUI();
-        }
-    }
 
     // hides status bar and navbar
     private void hideSystemUI() {
